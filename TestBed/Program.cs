e663176ee880fa71be7510268f48a79c51163d96
Henry Buckle <h.buckle@outlook.com>
2016-10-02 18:49:12 +0100

    add the testbed

---
+++ TestBed/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new AXLSharp.AXLPortClient();
+            service.ClientCredentials.UserName.UserName = "administrator";
+            service.ClientCredentials.UserName.Password = "ciscopsdt";
+
+            var getlinereq = new AXLSharp.GetLineReq();
+            getlinereq.Items = new object[1];
+            getlinereq.Items[0] = "1006";
+            getlinereq.ItemsElementName = new AXLSharp.ItemsChoiceType57[1];
+            getlinereq.ItemsElementName[0] = AXLSharp.ItemsChoiceType57.pattern;
+            var result = service.getLine(getlinereq);
+        }
+    }
+}
